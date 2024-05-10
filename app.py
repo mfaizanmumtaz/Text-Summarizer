50f6dcf543cd903ff60c3ed5870c04d359daa0ae
mfaizanmumtaz <mfaizanmumtaz999@gmail.com>
2024-05-10 06:05:01 +0500

    Text Summarizer

---
+++ app.py
@@ -0,0 +1,24 @@
+prompt_template = """Write a concise summary of the following:
+"{text}"
+CONCISE SUMMARY:"""
+
+from langchain.schema.output_parser import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+prompt = PromptTemplate.from_template(prompt_template)
+
+llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-pro")
+chain = prompt | llm | StrOutputParser()
+
+
+import streamlit as st
+st.set_page_config(page_icon=":pencil2:", page_title="Text Summarizer")
+st.title("Text Summarizer 📝")
+
+text = st.text_area("Enter text")
+if st.button("Generate Summary"):
+    with st.spinner("Generating summary..."):
+        response = chain.invoke(
+                {"text": text})
+        st.chat_message("assistant").write(response)
